2b6c50205645f5ae51fd8cd778ffd6e4ec173168
laurenmsv <lamiseriforgor@gmail.com>
2026-05-21 09:23:03 -0700

    MyProgram.java

---
+++ MyProgram.java
@@ -0,0 +1,22 @@
+import javax.swing.JFrame;
+
+public class MyProgram {
+    public static void main(String[] args) {
+     
+        
+         JFrame MainFrame = new JFrame();
+        
+        MainFrame.setSize(600,800);
+        MainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+     
+Panel panel = new Panel();
+  
+MainFrame.add(panel);
+           
+       MainFrame.setVisible(true);
+     
+     panel.requestFocus();
+     
+     
+    }
+}
